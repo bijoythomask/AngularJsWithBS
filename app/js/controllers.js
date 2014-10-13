81061d56b8d9d1424ce05b21646883760d97e3f5
bijoy <bijoy@bijoy-desktop>
2014-10-13 22:55:37 +0530

    Partially added the view 2 implermentaion

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,13 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+        .controller('MyCtrl1', ['$scope', function ($scope) {
 
-  }]);
+            }])
+        .controller('MyCtrl2', ['$scope','Phone', function ($scope,Phone) {
+                $scope.phones = Phone.query();
+                $scope.orderProp = 'age';
+            }]);
+
+     
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,12 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+var customerServices = angular.module('myApp.services', []).
+        value('version', '0.1');
+
+customerServices.factory('Phone', ['$resource',
+    function ($resource) {
+        return $resource('http://localhost:8080/CustomerDB/webresources/com.rest.customer.customer.json', {}, {
+            query: {method: 'GET', params: {phoneId: 'phones'}, isArray: true}
+        });
+    }]);
