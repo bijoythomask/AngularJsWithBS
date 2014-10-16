4c56add1a22478bf950f08865bb727072e50f789
bijoy <bijoy@bijoy-desktop>
2014-10-16 21:03:45 +0530

    Restructured UI of the application

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,21 +4,11 @@
 
 angular.module('myApp.controllers', [])
 
-        
-
-        .controller('MyCtrl1', ['$scope', function ($scope ) {
-                
-            }])
-        .controller('MyCtrl2', ['$scope','Customer', function ($scope,Customer) {
-                
+        .controller('MyCtrl1', ['$scope','Customer', function ($scope,Customer ) {
                 $scope.customers = Customer.get();
-               
-                $scope.testFn= function(){
-                    
-                    $scope.customers = Customer.get();
-                    console.log('inside get method');
-                    
-                };
+            }])
+        .controller('MyCtrl2', ['$scope', function ($scope) {
+                
                 
             }]);
         
